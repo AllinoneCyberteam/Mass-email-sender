ae2e2e7f2f3ec72ff1e150e29c7f1cca62bc4ce6
Gadila Shashank Reddy <shashankgadila@gmail.com>
2019-01-04 00:34:30 +0530

    Add comments

--- mailer.py
+++ mailer.py
@@ -2,14 +2,16 @@
 
 import json
 import smtplib
+import time
 
 EMAIL_LIST = []
 
+# Read config file
 with open("./CONFIG", "r") as file:
     CONFIG = json.loads(file.read())
 file.close()
 
-
+# Read mailing list and add it to EMAIL_LIST
 with open("./mailing_list.txt", "r") as file:
     while True:
         LINE = file.readline()
@@ -19,12 +21,17 @@ with open("./mailing_list.txt", "r") as file:
             EMAIL_LIST.append(LINE.strip())
 file.close()
 
+# Construct the message to be sent.
 MESSAGE = "SUBJECT: {} \n\n{}".format(CONFIG["subject"], CONFIG["message"])
 
+# Open SMTP secure connection.
 EMAIL_SERVER = smtplib.SMTP_SSL("smtp.gmail.com", "465")
+# Login
 EMAIL_SERVER.login(CONFIG["email"], CONFIG["password"])
+# Send emails to emails in EMAIL_LIST in a loop
 for email in EMAIL_LIST:
     EMAIL_SERVER.sendmail(CONFIG["email"], email, MESSAGE)
+    time.sleep(0.5)
 
-
+# Quit SMTP server
 EMAIL_SERVER.quit()
